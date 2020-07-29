294a895af8d7b427516b70d9b7e95d622c87f26e
Caleb Stripes <calebdstripes@gmail.com>
2020-07-29 12:16:50 -0400

    add descriptions

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,18 +1,16 @@
 // Project Animation function
 
 
+const descriptions = document.getElementsByClassName('description');
 
 function updateProjectsJs() {
     const panelOne = document.getElementById('panelOne')
     const panelTwo = document.getElementById('panelTwo')
     const panelThree = document.getElementById('panelThree')
-    
+
     panelOne.addEventListener('click', openOne);
     panelTwo.addEventListener('click', openTwo);
-    panelThree.addEventListener('click', openThree);
-
-    //make a button to run the toggleOff function
-    
+    panelThree.addEventListener('click', openThree);    
 }
             
 function openOne() {
@@ -23,6 +21,7 @@ function openOne() {
         panelOne.classList.toggle('open')
         panelTwo.classList.toggle('close')
         panelThree.classList.toggle('close')
+        descriptions[0].style.visibility = "visible";
     }
 }
 
@@ -30,10 +29,11 @@ function openTwo() {
     if (panelTwo.classList == 'panel open'){
         toggleOff();
     } else {
-    toggleOff();
-    panelOne.classList.toggle('close')
-    panelTwo.classList.toggle('open')
-    panelThree.classList.toggle('close')
+        toggleOff();
+        panelOne.classList.toggle('close')
+        panelTwo.classList.toggle('open')
+        panelThree.classList.toggle('close')
+        descriptions[1].style.visibility = "visible";
     }
 }
 
@@ -41,10 +41,11 @@ function openThree() {
     if (panelThree.classList == 'panel open'){
         toggleOff();
     } else {
-    toggleOff();
-    panelOne.classList.toggle('close')
-    panelTwo.classList.toggle('close')
-    panelThree.classList.toggle('open')
+        toggleOff();
+        panelOne.classList.toggle('close')
+        panelTwo.classList.toggle('close')
+        panelThree.classList.toggle('open')
+        descriptions[2].style.visibility = "visible";
     }
 }
 
@@ -52,6 +53,8 @@ function toggleOff() {
     const panels = [panelOne, panelTwo, panelThree]
     panels.forEach(panel => {
         panel.classList = 'panel';
-   });
-    
+    });
+    descriptions[0].style.visibility = "hidden";
+    descriptions[1].style.visibility = "hidden";
+    descriptions[2].style.visibility = "hidden";
 }
