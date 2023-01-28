5cf09d1f2df6e3b8590068258a062145c781d097
Caleb Stripes <calebdstripes@gmail.com>
2023-01-27 22:14:30 -0500

    added menu drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyButton from './components/button.js'
+import PersistentDrawerLeft from './components/navdrawer.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <PersistentDrawerLeft />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Portfolio Under Construction
